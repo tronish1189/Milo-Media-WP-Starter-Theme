922e7f44d583dc37549b02effe61b6a148d46904
Michael Lopetrone <michael@milomedia.co>
2019-06-30 13:32:41 -0400

    Added custom functions (e.g. Custom Dashboard widgets, custom admin footer, custom admin logo, show active page template)

--- functions.php
+++ functions.php
@@ -1,6 +1,80 @@
 <?php
 
-//*Enable Featured Images
+// Enable Featured Images
 add_theme_support( 'post-thumbnails' );
 
+// Add custom widgets to the dashboard.
+function milo_add_dashboard_widgets() {
+	wp_add_dashboard_widget(
+		'milo_documentation_widget', // Widget slug.
+		'Website Documentation', // Title.
+		'milo_documentation_dashboard_widget' // Display function.
+	);
+	wp_add_dashboard_widget(
+		'milo_contact_widget', // Widget slug.
+		'Technical Support', // Title.
+		'milo_contact_dashboard_widget' // Display function.
+	);
+}
+add_action( 'wp_dashboard_setup', 'milo_add_dashboard_widgets' );
+
+// Create Documentation admin widget
+function milo_documentation_dashboard_widget() {
+	echo "
+  <a target='_blank' href='https://docs.google.com/document/d/1fgOr3NEM_V0p6sdZhvzCieJHtaANIuDTRAm1QfH5_9s/edit?usp=sharing'>Google Doc</a> (only editable by Milo Media)
+  <br>
+  <a target='_blank' href='https://drive.google.com/file/d/1KGrgKyR5gTX6s7VEjlLAukzYET4Y9pQo/view?usp=sharing'>PDF</a>
+  ";
+}
+
+// Create Site Contact admin widget
+function milo_contact_dashboard_widget() {
+	echo "
+  For technical assistance with this website, please contact:<br>
+  <br>
+	Michael Lopetrone<br>
+	Milo Media<br>
+	<a href='https://www.milomedia.co'>https://www.milomedia.co</a><br>
+	313-327-2820
+  ";
+}
+
+// Custom Milo Media Admin Footer
+function milo_remove_footer_admin () {
+	echo '<span id="footer-thankyou">Built by <a href="https://www.milomedia.co" target="_blank">Milo Media</a></span>';
+}
+add_filter( 'admin_footer_text', 'milo_remove_footer_admin' );
+
+// Custom Admin Logo Header
+function milo_custom_login_logo()
+{
+    echo '<style  type="text/css"> h1 a {  background-image:url(' . get_bloginfo('template_directory') . '/img/tpf-logo-color.png)  !important;     background-size: 250px !important;     height: 130px !important;     width: 250px !important;} </style>';
+}
+add_action('login_head',  'milo_custom_login_logo');
+
+/**
+ * Adds a column in the admin dashboard to show the active page template
+ * @link https://www.isitwp.com/custom-column-with-currently-active-page-template/
+ */
+add_filter( 'manage_pages_columns', 'page_column_views' );
+add_action( 'manage_pages_custom_column', 'page_custom_column_views', 5, 2 );
+function page_column_views( $defaults )
+{
+   $defaults['page-layout'] = __('Template');
+   return $defaults;
+}
+function page_custom_column_views( $column_name, $id )
+{
+   if ( $column_name === 'page-layout' ) {
+       $set_template = get_post_meta( get_the_ID(), '_wp_page_template', true );
+       if ( $set_template == 'default' ) {
+           echo 'Default';
+       }
+       $templates = get_page_templates();
+       ksort( $templates );
+       foreach ( array_keys( $templates ) as $template ) :
+           if ( $set_template == $templates[$template] ) echo $template;
+       endforeach;
+   }
+}
 ?>
